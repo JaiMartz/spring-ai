d0beaf643d0ae697d9f31789530c5890dc7718e9
jaimartz <martinezgarridojairo@gmail.com>
2025-03-11 20:48:32 +0100

    OpenAIService

---
+++ src/main/java/jaimartz/dev/spring_ai/services/OpenAIService.java
@@ -0,0 +1,6 @@
+package jaimartz.dev.spring_ai.services;
+
+public interface OpenAIService {
+
+    String getAnswer(String question);
+}
---
+++ src/test/java/jaimartz/dev/spring_ai/services/OpenAIServiceImplTest.java
@@ -0,0 +1,20 @@
+package jaimartz.dev.spring_ai.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class OpenAIServiceImplTest {
+
+    @Autowired
+    OpenAIService openAIService;
+
+    @Test
+    void getAnswer() {
+        String answer = openAIService.getAnswer("Tell me a dad joke.");
+        System.out.println("Got the answer");
+        System.out.println(answer);
+    }
+}
